b72e0cfb55d68f6e3b40e0be5a2890747381c5a4
Uliana Afonina <afonina.uliana@otr.ru>
2022-04-15 10:58:07 +0300

    Initial commit

---
+++ src/main/java/com/afonina/converter/springboot_converter/SpringbootConverterApplication.java
@@ -0,0 +1,13 @@
+package com.afonina.converter.springboot_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afonina/converter/springboot_converter/SpringbootConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afonina.converter.springboot_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
