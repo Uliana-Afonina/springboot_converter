380443e2d652485b8f26968a9237b7f7d6ad4fab
Uliana Afonina <afonina.uliana@otr.ru>
2022-04-20 10:50:34 +0300

    создала getConversionResult() в контроллере

--- src/main/java/com/afonina/converter/springboot_converter/controller/MainController.java
+++ src/main/java/com/afonina/converter/springboot_converter/controller/MainController.java
@@ -2,6 +2,7 @@ package com.afonina.converter.springboot_converter.controller;
 
 import com.afonina.converter.springboot_converter.service.api.CurrencyRateService;
 import com.afonina.converter.springboot_converter.entity.*;
+import com.afonina.converter.springboot_converter.service.impl.ConvertService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,8 @@ public class MainController {
 
     @Autowired
     private CurrencyRateService currencyRateService;
+    @Autowired
+    private ConvertService convertService;
 
     @GetMapping("/currencyRates")
     public List<CurrencyRate> getAllCurrencyRates() {
@@ -30,6 +33,12 @@ public class MainController {
     public CurrencyRate getCurrencyRates(@PathVariable String charCode) {
         CurrencyRate currencyRate = currencyRateService.getCurrencyRateByCharCode(charCode);
         return currencyRate;
+    }
+
+    @GetMapping("/currencyRates")
+    public String getConversionResult(@RequestBody CurrencyRate sourceCurrency, CurrencyRate targetCurrency, String coefficient) {
+        String result = convertService.convert(sourceCurrency, targetCurrency, coefficient);
+        return result;
     }
 //    @GetMapping()
 //    public List<Currency> getAllCurrencies() {
